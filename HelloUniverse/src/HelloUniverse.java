207e9c089713daf68fe0c80d210fc7be467c3220
jacsebastien <jac.sebastien@gmail.com>
2020-05-07 16:39:27 +0200

    feat: 🎸 Project creation

---
+++ HelloUniverse/src/HelloUniverse.java
@@ -0,0 +1,15 @@
+public class HelloUniverse {
+
+    public static void main(String... args){
+        String text = "Aux dernières nouvelles, le nombre de planètes dans le système solaire est de :";
+        int numberOfPlanets = 8;
+        System.out.println(text);
+        System.out.println(numberOfPlanets);
+
+        text = "Il y a quelques années cependant, elles étaient au nombre de :";
+        numberOfPlanets = 9;
+        System.out.println(text);
+        System.out.println(numberOfPlanets);
+    }
+
+}
---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld {
+    public static void main (String... args) {
+        int ageDuCapitaine; // long / short / byte
+        float temperature; // double
+        char initialePrenom;
+        boolean javaEstFacile;
+
+        int anneeDeNaissance = 1989;
+        anneeDeNaissance = 1980;
+        long grosChiffre = 45L;
+        temperature = 1.44F;
+        initialePrenom = 'c';
+        javaEstFacile = true;
+
+        String helloWorld = "Hello World !";
+        System.out.println(helloWorld);
+    }
+}
