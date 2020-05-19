341f224af895aade5edd364793274cf16573289c
jacsebastien <jac.sebastien@gmail.com>
2020-05-19 16:58:19 +0200

    feat: 🎸 Use concatenation + condition + loop

--- HelloUniverse/src/HelloUniverse.java
+++ HelloUniverse/src/HelloUniverse.java
@@ -1,15 +1,17 @@
 public class HelloUniverse {
 
-    public static void main(String... args){
-        String text = "Aux dernières nouvelles, le nombre de planètes dans le système solaire est de :";
-        int numberOfPlanets = 8;
-        System.out.println(text);
-        System.out.println(numberOfPlanets);
-
-        text = "Il y a quelques années cependant, elles étaient au nombre de :";
-        numberOfPlanets++;
-        System.out.println(text);
-        System.out.println(numberOfPlanets);
+    public static void main(String... args) {
+        for (int numberOfPlanets = 8; numberOfPlanets < 11; numberOfPlanets++) {
+            switch (numberOfPlanets) {
+                case 8:
+                    System.out.println("Aux dernières nouvelles, le nombre de planètes dans le système solaire est de : " + numberOfPlanets);
+                    break;
+                case 9:
+                    System.out.println("Il y a quelques années cependant, elles étaient au nombre de : " + numberOfPlanets);
+                    break;
+                default:
+                    System.out.println("Au cours de l'ère moderne, le nombre de planètes n'a jamais été officiellement de : " + numberOfPlanets);
+            }
+        }
     }
-
 }
